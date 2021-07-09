d041847680b558fd1c6b2acd72c3d6c0f9ca7e0a
gaynor@illinois.edu <vnlH814i>
2021-07-09 15:57:32 -0500

    CIL-934 work on DB service.

--- cilogon2-server-loader-oauth2/src/main/java/org/cilogon/oauth2/servlet/impl/DBService2.java
+++ cilogon2-server-loader-oauth2/src/main/java/org/cilogon/oauth2/servlet/impl/DBService2.java
@@ -4,6 +4,7 @@ import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.OA2SE;
 import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.OA2ServiceTransaction;
 import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.servlet.OA2ClientUtils;
 import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.servlet.RFC8628Servlet;
+import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.servlet.RFC8628State;
 import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.state.ScriptRuntimeEngineFactory;
 import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.storage.clients.OA2Client;
 import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.storage.clients.OA2ClientApprovalKeys;
@@ -15,6 +16,7 @@ import edu.uiuc.ncsa.security.core.exceptions.NFWException;
 import edu.uiuc.ncsa.security.core.util.BasicIdentifier;
 import edu.uiuc.ncsa.security.core.util.DateUtils;
 import edu.uiuc.ncsa.security.core.util.DebugUtil;
+import edu.uiuc.ncsa.security.core.util.StringUtils;
 import edu.uiuc.ncsa.security.delegation.token.impl.AuthorizationGrantImpl;
 import edu.uiuc.ncsa.security.delegation.token.impl.TokenUtils;
 import edu.uiuc.ncsa.security.oauth_2_0.OA2Constants;
@@ -69,10 +71,12 @@ public class DBService2 extends AbstractDBService {
     public static final int STATUS_UNAPPROVED_CLIENT = 0x1000F; // 65551
     public static final int STATUS_NO_SCOPES = 0x10011; //65553
     public static final int STATUS_MALFORMED_SCOPE = 0x10013; //65555
+    public static final int STATUS_SERVICE_UNAVAILABLE = 0x10015; //65557
 
     public static final String CHECK_USER_CODE = "checkUserCode";
     public static final int CHECK_USER_CODE_CASE = 740;
     public static final String USER_CODE_PARAMETER = "userCode";
+    public static final String USER_CODE_APPROVED_PARAMETER = "approved";
     //public static final int STATUS_MISSING = 0x10013; //65555
 
 
@@ -111,6 +115,40 @@ public class DBService2 extends AbstractDBService {
         super.doAction(request, response, action);
     }
 
+    /**
+     * Taken from <a href="https://jira.ncsa.illinois.edu/browse/CIL-934">CIL-934</a>
+     * action: checkUserCode
+     * param: user_code (required, but can be empty)
+     * <p>
+     * Purpose:
+     * This is an "internal" dbService method used by the PHP web front end to
+     * (1) verify that a user_code input by the user is valid and
+     * (2) return the client_id associated with this transaction in order to display client
+     * information to the end user. The user_code parameter is required, but it can be empty.
+     * The user_code parameter can contain extra "user-friendly" characters such as
+     * dash '-', space ' ', underscore '_', etc. These extra characters will be stripped
+     * out/ignored by the dbService. The user_code can contain lower-case and/or
+     * upper-case characters which will be transformed to upper-case characters by the dbService.
+     * Returns: HTTP 200 response, body is basic text, one line per returned value:
+     * <p>
+     * status=INTEGER
+     * 0 = Success
+     * 1048569 = missing parameter
+     * 65537 = transaction not found
+     * 65539 = expired user_code (token)
+     * client_id=The OIDC client_id matching the user_code
+     * user_code=The original user_code to be displayed to the end user. The purpose of this
+     * is that the returned user_code should visually match the one that was returned to
+     * the device so the user can easily verify a match (i.e., ignore any
+     * transformations done by the user when inputting the user_code).
+     * scope=A (possibly empty/absent) space-separated list of scopes that were requested by
+     * the client. This is needed when displaying the list of attributes to be delegated
+     * since the scopes requested by the device client may differ from those registered.
+     *
+     * @param request
+     * @param response
+     * @throws IOException
+     */
     protected void checkUserCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
         if (!request.getParameterMap().containsKey(USER_CODE_PARAMETER)) {
             // missing parameter
@@ -118,24 +156,60 @@ public class DBService2 extends AbstractDBService {
             return;
         }
         String userCode = request.getParameter(USER_CODE_PARAMETER);
+        if (StringUtils.isTrivial(userCode)) {
+            doError("No user code parameter was found.", STATUS_MISSING_ARGUMENT, response);
+        }
+        userCode = RFC8628Servlet.convertToCanonicalForm(userCode);
+
         CILogonOA2ServiceEnvironment se = (CILogonOA2ServiceEnvironment) getServiceEnvironment();
-        if (se.isRfc8628Enabled() && RFC8628Servlet.getCache().containsKey(userCode)) {
-            AuthorizationGrantImpl ag = new AuthorizationGrantImpl(URI.create(RFC8628Servlet.getCache().get(userCode)));
+        if (!se.isRfc8628Enabled()) {
+            doError("Device flow is not available on this server.", STATUS_SERVICE_UNAVAILABLE, response);
+            return;
+        }
+        if (RFC8628Servlet.getCache().containsKey(userCode)) {
+            String rawAG = RFC8628Servlet.getCache().get(userCode);
+            if (StringUtils.isTrivial(rawAG)) {
+                doError("token not found.", STATUS_EXPIRED_TOKEN, response);
+            }
+
+            // It is possible that the transaction was garbage collected but the GC hasn't removed it
+            // from the cache, so we do have to check if the ag is expired.
+            AuthorizationGrantImpl ag = new AuthorizationGrantImpl(URI.create(rawAG));
             if (ag.isExpired()) {
-                writeUserCodeNotFound(response);
+                doError("token not found.", STATUS_EXPIRED_TOKEN, response);
                 return;
             }
             CILOA2ServiceTransaction transaction = (CILOA2ServiceTransaction) se.getTransactionStore().get(ag);
+
             if (transaction == null) {
                 // Then the pending transaction got garbage collected so it effectively timed out
-                writeUserCodeNotFound(response);
+                doError("transaction not found.", STATUS_TRANSACTION_NOT_FOUND, response);
                 return;
             }
+            if (!transaction.isRFC8628Request()) {
+                doError("invalid token.", STATUS_TRANSACTION_NOT_FOUND, response);
+                return;
+            }
+            String scopes = "";
+            if (!transaction.getScopes().isEmpty()) {
+                boolean firstPass = true;
+                for (String s : transaction.getScopes()) {
+                    if (firstPass) {
+                        firstPass = false;
+                        scopes = s;
+                    } else {
+                        scopes = scopes + " " + s;
+                    }
+                }
+            }
+
             startWrite(response);
             PrintWriter printWriter = response.getWriter();
-            printWriter.println(STATUS_KEY + "=1");
-            printWriter.println(CLIENT_ID + "=");
+            printWriter.println(STATUS_KEY + "=" + STATUS_OK);
+            printWriter.println(CLIENT_ID + "=" + transaction.getClient().getIdentifierString());
             printWriter.println("grant=" + TokenUtils.b32EncodeToken(ag.getToken()));
+            printWriter.println("scope=" + scopes);
+            printWriter.println("user_code=" + userCode);
             printWriter.flush();
             printWriter.close();
             stopWrite(response);
@@ -143,6 +217,109 @@ public class DBService2 extends AbstractDBService {
         }
 
         writeUserCodeNotFound(response);
+
+    }
+
+    /**
+     * action: userCodeApproved
+     * param(s):
+     * <p>
+     * user_code (required)
+     * approved (optional; defaults to 1; 1=approved; 0=denied)
+     * <p>
+     * Purpose: This is an "internal" dbService method used by the PHP web front end
+     * to let the dbService know that the user has logged on to their
+     * chosen Identity Provider and approved the transaction OR
+     * that the user has clicked a "Cancel" button and denied the transaction.
+     * If the user has approved the transaction (approved=1 or 'approved'
+     * is absent, the default), the OA4MP server can proceed with the
+     * rest of the Device authz grant flow. If the user has denied the
+     * transaction (approved=0), the OA4MP server should inform the device
+     * that the user has canceled the transaction.
+     * Returns: HTTP 200 response, body is basic text, one line per returned value:
+     * <p>
+     * status=INTEGER
+     * 0 = Success
+     * 1048569 = missing parameter
+     * 65537 = transaction not found
+     * 65539 = expired user_code (token)
+     */
+    protected void userCodeApproved(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (!request.getParameterMap().containsKey(USER_CODE_PARAMETER)) {
+            // missing parameter
+            doError("No user code parameter was found.", STATUS_MISSING_ARGUMENT, response);
+            return;
+        }
+        int approved = 1; // default
+        if (request.getParameterMap().containsKey(USER_CODE_APPROVED_PARAMETER)) {
+            try {
+                approved = Integer.parseInt(request.getParameter(USER_CODE_APPROVED_PARAMETER));
+            } catch (NumberFormatException nfx) {
+                doError("unknown value for " +
+                                USER_CODE_APPROVED_PARAMETER + " parameter \"" +
+                                request.getParameter(USER_CODE_APPROVED_PARAMETER) + "\"",
+                        STATUS_MISSING_ARGUMENT, response);
+            }
+        }
+        String userCode = request.getParameter(USER_CODE_PARAMETER);
+        if (StringUtils.isTrivial(userCode)) {
+            doError("No user code parameter was found.", STATUS_MISSING_ARGUMENT, response);
+        }
+        userCode = RFC8628Servlet.convertToCanonicalForm(userCode);
+
+        CILogonOA2ServiceEnvironment se = (CILogonOA2ServiceEnvironment) getServiceEnvironment();
+        if (!se.isRfc8628Enabled()) {
+            doError("Device flow is not available on this server.", STATUS_SERVICE_UNAVAILABLE, response);
+            return;
+        }
+        if (RFC8628Servlet.getCache().containsKey(userCode)) {
+            String rawAG = RFC8628Servlet.getCache().get(userCode);
+            if (StringUtils.isTrivial(rawAG)) {
+                doError("token not found.", STATUS_EXPIRED_TOKEN, response);
+            }
+
+            // It is possible that the transaction was garbage collected but the GC hasn't removed it
+            // from the cache, so we do have to check if the ag is expired.
+            AuthorizationGrantImpl ag = new AuthorizationGrantImpl(URI.create(rawAG));
+            if (ag.isExpired()) {
+                doError("token not found.", STATUS_EXPIRED_TOKEN, response);
+                writeUserCodeNotFound(response);
+                return;
+            }
+            CILOA2ServiceTransaction transaction = (CILOA2ServiceTransaction) se.getTransactionStore().get(ag);
+            if (transaction == null) {
+                // Then the pending transaction got garbage collected so it effectively timed out
+                doError("transaction not found.", STATUS_TRANSACTION_NOT_FOUND, response);
+                return;
+            }
+            if (!transaction.isRFC8628Request()) {
+                doError("invalid token.", STATUS_TRANSACTION_NOT_FOUND, response);
+                return;
+            }
+
+            RFC8628State rfc8628State = transaction.getRFC8628State();
+            RFC8628Servlet.getCache().remove(userCode);
+
+            if(approved == 1){
+                rfc8628State.valid = true; // means they actually logged in
+
+            } else{
+                // means they cancelled the whole thing. Remove the transaction
+                getTransactionStore().remove(transaction.getIdentifier());
+            }
+            // The JSON library copies everything no matter what, so no guarantee what's in the transaction is the same object.
+            // Just replace it with the good copy.
+            startWrite(response);
+            PrintWriter printWriter = response.getWriter();
+            printWriter.println(STATUS_KEY + "=" + STATUS_OK);
+            printWriter.println(CLIENT_ID + "=" + transaction.getClient().getIdentifierString());
+            printWriter.println("grant=" + TokenUtils.b32EncodeToken(ag.getToken()));
+            printWriter.println("user_code=" + userCode);
+            printWriter.flush();
+            printWriter.close();
+            stopWrite(response);
+            return;
+        }
 
     }
 
