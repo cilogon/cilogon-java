6d9bee5f0a9331ee1065ecc816e0d7a0faf29dd4
gaynor@illinois.edu <vnlH814i>
2022-07-27 13:35:42 -0500

    Poms updated to get qdl from new repo, build and build-all scripts completely redone and added

---
+++ build-all.sh
@@ -0,0 +1,50 @@
+# Top-level build script for everything. This build NCSA, QDL, OA4MP then CILogon.
+
+DEPLOY_ROOT=/home/ncsa/dev/temp-deploy
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+SVN_ROOT=/home/ncsa/dev/ncsa-git
+CILOGON_OA2_DEPLOY=$DEPLOY_ROOT/cilogon-oa2
+OA4MP_CLIENT_DEPLOY=$DEPLOY_ROOT/client
+OA4MP_SERVER_DEPLOY=$DEPLOY_ROOT/server
+
+echo "cleaning out old deploy in " $DEPLOY_ROOT
+if [ ! -d "$DEPLOY_ROOT" ]; then
+    mkdir "$DEPLOY_ROOT"
+fi
+cd $DEPLOY_ROOT
+rm -Rf *
+
+if [ $? -ne 0 ]; then
+    echo "Remove failed. Exiting build"
+    exit
+fi
+echo "Files removed, making target directories"
+
+mkdir "$OA4MP_CLIENT_DEPLOY"
+mkdir "$OA4MP_SERVER_DEPLOY"
+mkdir "$CILOGON_OA2_DEPLOY"
+mkdir "$DEPLOY_ROOT/oa2-qdl"
+
+echo "Target directories created"
+
+/home/ncsa/dev/ncsa-git/security-lib/build.sh
+if [ $? -ne 0 ]; then
+    echo "NCSA build failed. Exiting build"
+    exit
+fi
+/home/ncsa/dev/ncsa-git/qdl/build.sh
+if [ $? -ne 0 ]; then
+    echo "QDL build failed. Exiting build"
+    exit
+fi
+/home/ncsa/dev/ncsa-git/oa4mp/build.sh
+if [ $? -ne 0 ]; then
+    echo "OA4MP build failed. Exiting build"
+    exit
+fi
+
+/home/ncsa/dev/ncsa-git/cilogon/build.sh
+if [ $? -ne 0 ]; then
+    echo "CILogon build failed. Exiting build"
+    exit
+fi
---
+++ build.sh
@@ -0,0 +1,34 @@
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+DEPLOY_ROOT=/home/ncsa/dev/temp-deploy
+SVN_ROOT=/home/ncsa/dev/ncsa-git
+CILOGON_ROOT=$SVN_ROOT/cilogon
+CILOGON_OA2_DEPLOY=$DEPLOY_ROOT/cilogon-oa2
+CILOGON_OA2_TOOLS=$CILOGON_ROOT/cilogon-admin
+
+cd $CILOGON_ROOT
+mvn clean install
+
+if [[ $? -ne 0 ]] ; then
+    echo "CILogon maven build failed, exiting..."
+    exit 1
+fi
+
+cd $CILOGON_OA2_TOOLS
+
+mvn -P cli package
+cp $CILOGON_ROOT/cilogon-server/target/oauth2.war $CILOGON_OA2_DEPLOY/
+cp $CILOGON_ROOT/cilogon-client/target/cilogon-oa2-client.war $CILOGON_OA2_DEPLOY/
+
+cp $CILOGON_OA2_TOOLS/src/main/scripts/* $CILOGON_OA2_DEPLOY
+cp $CILOGON_OA2_TOOLS/src/main/resources/*.sql $CILOGON_OA2_DEPLOY
+
+
+cp cilogon-oa2-cli-jar-with-dependencies.jar $CILOGON_OA2_DEPLOY/cilogon-oa2-cli.jar
+cd $CILOGON_OA2_DEPLOY
+
+#CILogon QDL support
+
+cd $CILOGON_ROOT/cilogon-qdl
+mvn -P qdl package
+cd target
+cp cil-qdl-jar-with-dependencies.jar $CILOGON_OA2_DEPLOY/cil-qdl.jar
